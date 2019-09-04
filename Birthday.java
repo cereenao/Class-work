e51f03a9861cbbf269d70ddd2c6a639de80145c0
cereenao <54909809+cereenao@users.noreply.github.com>
2019-09-04 12:25:02 -0700

    Add files via upload

---
+++ Birthday.java
@@ -0,0 +1,27 @@
+/**
+ * Skeleton of a program that takes in the user's birth month and determines the number of days in the month
+ *
+ * @author Roy Chancellor
+ * @version 2/6/2019
+ */
+
+import java.util.Scanner;  //enables user input
+
+public class Birthday
+{
+    public static void main( String[] args) {
+        //variable declarations
+        int x;
+        
+        //get user input
+        Scanner keys = new Scanner(System.in);
+        System.out.println("Enter your birth month");  //prompt the user for a birth month
+        x = keys.nextInt();
+        keys.close();
+        
+        //determine the number of days in the user's birth month
+        if (x = 
+        //output the result to the user
+        System.out.println("");
+    }
+}
